c72bc401d6b2ebd7a82c686303c1663949af886f
super-lin0 <3924578651@qq.com>
2020-01-09 01:23:10 +0800

    fix webhooks

--- webhooks.js
+++ webhooks.js
@@ -34,24 +34,24 @@ handler.on("error", function(err) {
 });
 
 handler.on("*", function(event) {
-  console.log("Received *", event.payload.action);
+  console.log("Received *", event.payload);
   //   run_cmd('sh', ['./deploy-dev.sh'], function(text){ console.log(text) });
 });
 
-// handler.on("push", function(event) {
-//   console.log(
-//     "Received a push event for %s to %s",
-//     event.payload.repository.name,
-//     event.payload.ref
-//   );
-//   // 分支判断
-//   if (event.payload.ref === "refs/heads/master") {
-//     console.log("deploy master..");
-//     run_cmd("sh", ["./deploy-dev.sh"], function(text) {
-//       console.log(text);
-//     });
-//   }
-// });
+handler.on("push", function(event) {
+  console.log(
+    "Received a push event for %s to %s",
+    event.payload.repository.name,
+    event.payload.ref
+  );
+  // 分支判断
+  if (event.payload.ref === "refs/heads/master") {
+    console.log("deploy master..");
+    run_cmd("sh", ["./deploy-dev.sh"], function(text) {
+      console.log(text);
+    });
+  }
+});
 
 // handler.on("issues", function(event) {
 //   console.log(
